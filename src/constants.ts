91bee03eb6dd1ef20b24cf8a8a7ceb6b759c1888
osyrisrblx <osyrisrblx@gmail.com>
2024-08-28 21:43:39 -0400

    Remove unused JSON_EXT constant

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,6 @@ export const D_EXT = ".d";
 export const DTS_EXT = D_EXT + TS_EXT;
 export const TRANSFORMED_EXT = ".transformed";
 export const LUA_EXT = ".lua";
-export const JSON_EXT = ".json";
 
 export const INDEX_NAME = "index";
 export const INIT_NAME = "init";
